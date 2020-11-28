298c0c72db979c2de7716ec6785f56537b91cdb3
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-11-28 16:35:35 +0700

    add course and course adapter

---
+++ app/src/main/java/com/mobileprog/blajarinvest/CourseAdapter.java
@@ -0,0 +1,76 @@
+package com.mobileprog.blajarinvest;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<Course> courseList;
+
+    public CourseAdapter(Context context, ArrayList<Course> courseList) {
+        this.context = context;
+        this.courseList = courseList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_course, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Course course = courseList.get(position);
+        holder.tvName.setText(course.getName());
+//        holder.ivCourse.setImageResource(course.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return courseList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        TextView tvName;
+//        ImageView ivCourse;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+//            ivCourse = itemView.findViewById(R.id.ivCourse);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+//            int position = getAdapterPosition();
+//            Course course = courseList.get(position);
+
+//            Intent intent = new Intent(context, TargetActivity.class);
+
+//            intent.putExtra("course", course);
+
+//            context.startActivity(intent);
+
+        }
+
+
+    }
+}
+
+
--- app/src/main/java/com/mobileprog/blajarinvest/MainActivity.java
+++ app/src/main/java/com/mobileprog/blajarinvest/MainActivity.java
@@ -1,15 +1,42 @@
 package com.mobileprog.blajarinvest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Course> courseList = new ArrayList<>();
+    RecyclerView rvCourses;
+    CourseAdapter adapter;
+    TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        String username = PreferenceHelper.getUsername(this);
+        tvWelcome.setText("Halo, " + username);
+
+
+
+        courseList.add(new Course(1, "Introduksi"));
+
+        rvCourses = findViewById(R.id.rvCourses);
+        rvCourses.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new CourseAdapter(this, (ArrayList<Course>) courseList);
+        rvCourses.setAdapter(adapter);
+
     }
 }
