a8ec4c399b99660952a19ccc323bdf4ac7ae028c
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-12-18 00:02:52 +0700

    add more courses

--- app/src/main/java/com/mobileprog/blajarinvest/SplashActivity.java
+++ app/src/main/java/com/mobileprog/blajarinvest/SplashActivity.java
@@ -84,5 +84,27 @@ public class SplashActivity extends AppCompatActivity {
                         "\n\nModerat: Mengambil instrumen investasi secara mixed, memiliki toleransi terhadap kerugian. " +
                         "\n\nAgresif: Mengambil instrumen investasi yang memiliki risiko terbesar namun dengan potensi return yang lebih tinggi, memiliki toleransi yang besar terhadap kerugian.",
                 3));
+        courseDatabase.insertCourse(new Course("Jangan Serakah"));
+        courseContentDatabase.insertCourseContent(new CourseContent(4,
+                "Dalam melakukan investasi, penting untuk mengingatkan diri agar jangan serakah. Apabila ada peluang investasi dengan potensi return besar, maka tentu risiko yang ada juga besar. " +
+                        "\n\nBanyak sekali orang lupa akan hal tersebut dan akhirnya stress ketika mengalami kerugian, hal ini karena orang hanya melihat potensi return-nya saja tanpa memikirkan risiko yang pasti ada",
+                1));
+        courseDatabase.insertCourse(new Course("Instrumen Investasi"));
+        courseContentDatabase.insertCourseContent(new CourseContent(5,
+                "Insturmen investasi yang ada itu sangat banyak. Beberapa contohnya adalah:" +
+                        "\n1. Reksadana" +
+                        "\n2. Obligasi" +
+                        "\n3. Saham" +
+                        "\n4. Logam Mulia (cth: emas)" +
+                        "\n5. P2P Lending" +
+                        "\n\nSetiap instrumen invesstasi memiliki risiko yang berbeda, misalnya saham merupakan instrumen investasi dengan risiko yang cukup besar," +
+                        "apalagi jika Anda tidak menganalisa saham perusahaan yang Anda beli.",
+                1));
+        courseDatabase.insertCourse(new Course("Mulai Investasi"));
+        courseContentDatabase.insertCourseContent(new CourseContent(6,
+                "Tergantung instrumen investasi yang diinginkan, maka platform untuk melakukan investasi akan berbeda." +
+                        "\n\nUntuk investasi saham, maka dapat dimulai dengan membuka akun di sekuritas. Ada baiknya melakukan pengecekan di BEI dan memilih salah satu sekuritas terbesar yang ada di Indonesia, serta pertimbangkan juga sekuritas yang ada di dekat lokasi rumah." +
+                        "\n\nUntuk investasi reksadana, maka dapat dimulai dengan membuka akun di APERD (agen penjual reksa dana) yang terdaftar secara legal.",
+                1));
     }
 }
